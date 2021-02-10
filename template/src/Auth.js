c8fd423e32b9c398bbc6d4ed6b650db8d60e2b9d
gffuma <fumagalli.gf@gmail.com>
2021-02-10 14:32:46 +0100

    Modernize template inmagik use next rj version

--- template/src/Auth.js
+++ template/src/Auth.js
@@ -10,17 +10,18 @@ const loginCall = (params) =>
     .post('/auth-token/', params)
 const meCall = (token) => api.auth(token).get('/profile')
 
+function InnerAuth({ children }) {
+  const { callAuthApiObservable } = useAuthActions()
+  return (
+    <ConfigureRj effectCaller={callAuthApiObservable}>{children}</ConfigureRj>
+  )
+}
+
 function Auth({ children }) {
   return (
-    <EazyAuth
-      loginCall={loginCall}
-      meCall={meCall}
-      render={({ callAuthApiObservable }) => (
-        <ConfigureRj effectCaller={callAuthApiObservable}>
-          {children}
-        </ConfigureRj>
-      )}
-    />
+    <EazyAuth loginCall={loginCall} meCall={meCall}>
+      <InnerAuth />
+    </EazyAuth>
   )
 }
 
--- template/src/state/entity.js
+++ template/src/state/entity.js
@@ -5,28 +5,37 @@ import rjList, {
 import api from '../api'
 import { PAGE_SIZE } from '../consts'
 
-export const ListState = rj(rjList({
-  pagination: nextPreviousPaginationAdapter,
-  pageSize: PAGE_SIZE,
-}), {
-  mutations: {
-    removeEntity: {
-      effect: (t) => (entity) => api.auth(t)
-        .mapResponse(() => entity)
-        .delete(`/entity/${entity.id}`),
-      updater: 'deleteItem',
-      // Uncomment to make delete optimistic
-      // optimistiResult: entity => entity,
-    }
-  },
-  name: 'List',
-  effectCaller: rj.configured(),
-  effect: (t) => (params = {}) => api.auth(t).get('/entity', params),
-})
+export const ListState = rj(
+  rjList({
+    pagination: nextPreviousPaginationAdapter,
+    pageSize: PAGE_SIZE,
+  }),
+  {
+    mutations: {
+      removeEntity: {
+        effect: (t) => (entity) =>
+          api
+            .auth(t)
+            .mapResponse(() => entity)
+            .delete(`/entity/${entity.id}`),
+        updater: 'deleteItem',
+        // Uncomment to make delete optimistic
+        // optimistiResult: entity => entity,
+      },
+    },
+    name: 'List',
+    effectCaller: 'configured',
+    effect: (t) => (params = {}) => api.auth(t).get('/entity', params),
+    computed: {
+      pagination: 'getPagination',
+      list: 'getList',
+    },
+  }
+)
 
 export const DetailState = rj({
   name: 'Detail',
-  effectCaller: rj.configured(),
+  effectCaller: 'configured',
   mutations: {
     updateEntity: {
       effect: (t) => (id, entity) => api.auth(t).put(`/entity/${id}`, entity),
@@ -38,6 +47,6 @@ export const DetailState = rj({
 
 export const CreateState = rj({
   name: 'Create',
-  effectCaller: rj.configured(),
-  effect: t => (entity) => api.auth(t).post('/entity', entity)
+  effectCaller: 'configured',
+  effect: (t) => (entity) => api.auth(t).post('/entity', entity),
 })
